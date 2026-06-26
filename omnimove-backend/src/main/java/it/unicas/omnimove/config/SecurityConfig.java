0f7db43a69f772a471b14cb26bcf843abae2ead7
ferra <ferran.montero@studentmail.unicas.it>
2026-06-26 20:03:34 +0200

    Fixed recover password

--- omnimove-backend/src/main/java/it/unicas/omnimove/config/SecurityConfig.java
+++ omnimove-backend/src/main/java/it/unicas/omnimove/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/", "/error", "/omnimove-login.html", "/reset-password.html",
                                 "/favicon.ico", "/omnimove-login.css", "/omnimove-login.js",
-                                "/reset-password.css","/reset-password.js"
+                                "/reset-password.css","/reset-password.js", "/api/v1/auth/reset-page"
                         ).permitAll()
                         .requestMatchers(
                                 "/api/v1/auth/login",
